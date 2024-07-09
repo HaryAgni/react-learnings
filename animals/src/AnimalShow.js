010734ea6c74a15fbfa150ab9ee85e08b462aae9
Harshit Agnihotri <haryagni123@gmail.com>
2024-07-09 23:52:56 +0530

    added animal svg icons

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,5 +1,36 @@
-function AnimalShow({type}){
-    return <div>{type}</div>
+import { useState } from "react";
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);;
+  };
+  return (
+    <div onClick={handleClick}>
+      <img alt="animal" src={svgMap[type]} />
+      <img alt="heart" 
+      src={heart} 
+      style={{width: 10 + 10*clicks}}
+      />
+    </div>
+  );
 }
 
 export default AnimalShow;
