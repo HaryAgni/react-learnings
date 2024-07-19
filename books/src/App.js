01513746a092873bda70e04e3adec434ac68953b
Harshit Agnihotri <haryagni123@gmail.com>
2024-07-19 23:42:19 +0530

    post API integerated

--- books/src/App.js
+++ books/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -23,11 +24,9 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", { title });
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
