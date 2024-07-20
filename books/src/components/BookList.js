5d681b498a1e4062bce1a4be4b658b434056d78d
Harshit Agnihotri <haryagni123@gmail.com>
2024-07-20 23:05:03 +0530

    added context and provider

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,19 +1,23 @@
+import { useContext } from "react";
 import BookShow from "./BookShow";
+import BooksContext from "../context/books";
 
-function BookList({books, onDelete, onEdit}){
+function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
 
-    const renderedBooks = books.map(
-        (book) => {
-            return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
-        }
-    )
-
+  const renderedBooks = books.map((book) => {
     return (
-        <div className="book-list">
-            {renderedBooks}
-        </div>
+      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     );
+  });
 
+  return (
+    <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>click</button>
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
--- books/src/index.js
+++ books/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Provider } from "./context/books";
 
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App/>);
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
