e7f225ffac61fc00b07f732793ab7cd82adc5240
Harshit Agnihotri <haryagni123@gmail.com>
2024-07-21 20:04:56 +0530

    making use of prop-types library

---
+++ comps/src/Button.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+
+function Button({
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded
+}){
+    return <button>{children}</button>;
+}
+
+
+Button.propTypes ={
+    checkVariationValue: ({primary, secondary, success, warning, danger}) =>{
+        const count = Number(!!primary)
+        +Number(!!secondary)
+        +Number(!!warning)
+        +Number(!!success)
+        +Number(!!danger)
+
+        if(count>1){
+            return new Error('Only one of primary, secondary, success, warning, danger can be true')
+        }
+    }
+}
+
+export default Button;
