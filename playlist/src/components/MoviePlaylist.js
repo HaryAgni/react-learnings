af0ea62abdf53c48bd1967dd2c5b0526098c9026
Harshit Agnihotri <haryagni123@gmail.com>
2024-08-02 23:48:00 +0530

    added functionality to add and remove state using redux

--- playlist/src/components/MoviePlaylist.js
+++ playlist/src/components/MoviePlaylist.js
@@ -1,26 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
 import { createRandomMovie } from "../data";
+import { addMovie, removeMovie } from "../store";
 
 function MoviePlaylist() {
+  const dispatch = useDispatch();
   // To Do:
   // Get list of movies
-  const moviePlaylist = [];
+  const moviePlaylist = useSelector((state) => {
+    return state.movies;
+  });
 
   const handleMovieAdd = (movie) => {
     // To Do:
     // Add movie to list of movies
+    dispatch(addMovie(movie));
   };
   const handleMovieRemove = (movie) => {
     // To Do:
     // Remove movie from list of movies
+    dispatch(removeMovie(movie));
   };
 
   const renderedMovies = moviePlaylist.map((movie) => {
     return (
-      <li key={movie}>
+      <li className="box" key={movie}>
         {movie}
         <button
           onClick={() => handleMovieRemove(movie)}
-          className="button is-danger"
+          className="button is-danger is-pulled-right"
         >
           X
         </button>
@@ -30,9 +37,9 @@ function MoviePlaylist() {
 
   return (
     <div className="content">
-      <div className="table-header">
-        <h3 className="subtitle is-3">Movie Playlist</h3>
-        <div className="buttons">
+      <div className="table-header is-flex">
+        <h3 className="subtitle m-0 is-3">Movie Playlist</h3>
+        <div className="buttons ml-auto">
           <button
             onClick={() => handleMovieAdd(createRandomMovie())}
             className="button is-link"
--- playlist/src/store/index.js
+++ playlist/src/store/index.js
@@ -1,21 +1,50 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createAction, createSlice } from "@reduxjs/toolkit";
+const reset = createAction("app/reset");
+const movieSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+  extraReducers(builder){
+    builder.addCase(reset,(state,action)=>{
+        return [];
+    })
+  }
+});
 
 const songsSlice = createSlice({
   name: "song",
   initialState: [],
   reducers: {
     addSong(state, action) {
-        state.push(action.payload);
+      state.push(action.payload);
     },
-    removeSong(state, action) {},
+    removeSong(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(reset , (state, action) => {
+      return [];
+    });
   },
 });
 
 const store = configureStore({
-    reducer:{
-        songs: songsSlice.reducer
-    }
-})
+  reducer: {
+    songs: songsSlice.reducer,
+    movies: movieSlice.reducer,
+  },
+});
 
-export {store};
-export const {addSong} = songsSlice.actions;
+export { store, reset };
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = movieSlice.actions;
